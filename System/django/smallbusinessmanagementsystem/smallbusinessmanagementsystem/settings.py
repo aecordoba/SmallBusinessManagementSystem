a64ba4dc753989badf46e5796e5169ec4fedb2da
Adrián E. Córdoba <software.dynamicmcs@gmail.com>
2026-05-27 16:26:02 -0300

    Access for any host.

--- System/django/smallbusinessmanagementsystem/smallbusinessmanagementsystem/settings.py
+++ System/django/smallbusinessmanagementsystem/smallbusinessmanagementsystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-#y=i-1sb(^rlex@q_u^m@8217ncrgd(ib#pj)rtyr7-)a!bzv5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
